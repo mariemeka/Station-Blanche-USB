b6d1fe8a91185ad8ba4ade19d88eeb4a6f9d2db0
Antonin <carpentier.antonin.1@gmail.com>
2023-12-06 12:06:11 +0100

    added the integrity control after scan with sha256

--- station-blanche/scan_and_log.sh
+++ station-blanche/scan_and_log.sh
@@ -20,3 +20,9 @@ if [ $? -eq 0 ]; then
 else
     echo "USB keys may be infected. Scan results stored in $LOG_FILE."
 fi
+
+# fast method (we scan only files)
+sudo find $USB_DIRECTORY -type f -exec sha256sum {} + | sha256sum | tr -d '-' >> $LOG_FILE
+
+# slow method (scans all the device)
+#sudo dd if=$USB_DIRECTORY bs=4M status=progress | sha256sum | tr -d '-' >> $LOG_FILE
