1f7bc13e37081aed5cb967f516376f07fbb56882
malojeu <148615830+malojeu@users.noreply.github.com>
2023-12-06 10:57:54 +0100

    Create scan_and_log.sh

---
+++ station-blanche/scan_and_log.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Ensure the USB directory is mounted
+USB_DIRECTORY="/scanned_files"
+if [ ! -d "$USB_DIRECTORY" ]; then
+    echo "USB directory not found."
+    exit 1
+fi
+
+# Log file for scan results
+LOG_FILE="/scanned_files/scan_results.txt"
+
+# Perform ClamAV scan
+echo "Starting ClamAV scan..."
+clamscan -r "$USB_DIRECTORY" --log="$LOG_FILE"
+
+# Check the scan results
+if [ $? -eq 0 ]; then
+    echo "USB keys are clean. Scan results stored in $LOG_FILE."
+else
+    echo "USB keys may be infected. Scan results stored in $LOG_FILE."
+fi
+
+# Run your application or additional tasks if needed
+
+# Keep the container running
+tail -f /dev/null
