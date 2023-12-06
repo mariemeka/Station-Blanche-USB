693449190065011d73b3688cd71bbd97d5ab2dc4
malojeu <148615830+malojeu@users.noreply.github.com>
2023-12-06 10:55:56 +0100

    Update auto_mount.sh
    - added hardpath for device mounting point since we'll restrict usb devices to 1.
    
    - changed mount point

--- station-blanche/auto_mount.sh
+++ station-blanche/auto_mount.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+ede#!/bin/bash
 
 # Wait for the device to settle
 sleep 2
 
 # Get the device path
-DEVICE_PATH="/dev/$1"
+DEVICE_PATH="/dev/sdb"
 
 # Mount the USB device
-MOUNT_POINT="/scanned_files/$(basename $DEVICE_PATH)"
+MOUNT_POINT="/mnt/$(basename $DEVICE_PATH)"
 mkdir -p "$MOUNT_POINT"
 mount "$DEVICE_PATH" "$MOUNT_POINT"
