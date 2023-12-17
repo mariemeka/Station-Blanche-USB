51887c8dfdf623643d2810e2ade8ccd293c417a9
malojeu <148615830+malojeu@users.noreply.github.com>
2023-12-17 15:50:34 +0100

    Update auto_mount.sh
    amélioration 
     - vérification point de montage
     - montage
     - démontage

--- station-blanche/auto_mount.sh
+++ station-blanche/auto_mount.sh
@@ -1,12 +1,59 @@
-ede#!/bin/bash
+#!/bin/bash
 
-# Wait for the device to settle
-sleep 2
+ACTION=$1
+DEVBASE=$2
+DEVICE="/dev/${DEVBASE}"
+MOUNT_POINT="/mnt/usb"
+CHECK_MOUNT_POINT=$(mount | grep "${DEVICE}" | awk '{ print $3 }')
+DATE=$(date +"[%Y-%m-%d %H:%M:%S]")
 
-# Get the device path
-DEVICE_PATH="/dev/sdb"
+# MOUNT KEY
+mount_key() {
+    # Check if already mounted
+    if [[ -n "${CHECK_MOUNT_POINT}" ]]; then
+        exit 1
+    fi
 
-# Mount the USB device
-MOUNT_POINT="/mnt/$(basename $DEVICE_PATH)"
-mkdir -p "$MOUNT_POINT"
-mount "$DEVICE_PATH" "$MOUNT_POINT"
+    # Get info: id_fs_label, id_fs_uuid, and id_fs_type
+    eval "$(blkid -o udev "${DEVICE}")"
+    LABEL="${ID_FS_LABEL}"
+
+    if [[ -z "${LABEL}" ]]; then
+        LABEL="${DEVBASE}"
+    elif grep -q "${MOUNT_POINT}" /etc/mtab; then
+        LABEL+="-${DEVBASE}"
+    fi
+
+    # GLOBAL
+    OPTS="rw,relatime"
+    TYPE="vfat"
+
+    # MOUNT
+    mount -t "${TYPE}" -o "${OPTS}" "${DEVICE}" "${MOUNT_POINT}"
+
+    # LOG ACTION
+    echo "$DATE [USB][Mount] USB key mounted in '/mnt/usb'" >> /home/user/logs/usb.log
+}
+
+# UNMOUNT KEY
+unmount_key() {
+    if [[ -n "${CHECK_MOUNT_POINT}" ]]; then
+        # UNMOUNT
+        umount -l "${MOUNT_POINT}"
+
+        # LOG ACTION
+        echo "$DATE [USB][Unmount] USB key unmounted from '/mnt/usb'" >> /home/user/logs/usb.log
+    fi
+}
+
+# MAIN
+echo "$DATE [USB][Detect] USB key detected" >> /home/user/logs/usb.log
+
+case "${ACTION}" in
+    add)
+        mount_key
+        ;;
+    remove)
+        unmount_key
+        ;;
+esac
